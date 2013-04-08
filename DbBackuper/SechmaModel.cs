110ebe7f64c37de3f4ec9a89a6f9112159781556
AndyYou <andyyu0920@gmail.com>
2013-04-08 14:05:45 +0800

    add sechma model

---
+++ DbBackuper/SechmaModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbBackuper
+{
+    public class SechmaModel
+    {
+        public string TableName { get; set; }
+        public string Scripts { get; set; }
+        public List<ForeignKey> Foreignkeys { get; set; }
+        public string IndexScripts { get; set; }
+        public bool IsExistsOnTarget { get; set; }
+    }
+
+    public class ForeignKey
+    {
+        public string Table { get; set; }
+        public string Column { get; set; }
+    }
+}
